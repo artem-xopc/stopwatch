59e205a2117a4b7ff1a5178d00785e91bcfb72f1
artem-xopc <romanovartem.2000@mail.ru>
2023-01-19 14:36:02 +0300

    added final functional for stopwatch

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,17 +4,22 @@ const secElement = document.querySelector(".sec");
 const minElement = document.querySelector(".min");
 const hourElement = document.querySelector(".hour");
 
+const roundElement = document.querySelector(".round__input") 
+
 // Задаём кнопки
 const startButton = document.querySelector(".start");
 const pauseButton = document.querySelector(".pause");
 const stopButton = document.querySelector(".stop");
+const roundButton = document.querySelector(".new");
 
 // Объявляем основные переменные
 let milsec = 0,
   sec = 0,
   min = 0,
   hour = 0,
-  interval;
+  interval,
+  count = 0,
+  disabled = true;
 
 // задаём слушатели
 startButton.addEventListener("click", () => {
@@ -23,21 +28,50 @@ startButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", () => {
-    clearInterval(interval)
+  clearInterval(interval);
 });
 
 stopButton.addEventListener("click", () => {
-    clearInterval(interval);
-    milsec = 0
-    sec = 0
-    min = 0
-    hour = 0
-    milsecElement.textContent = "00"
-    secElement.textContent = "00"
-    minElement.textContent = "00"
-    hourElement.textContent = "00"
+  clearInterval(interval);
+  funcstopButton();
 });
 
+roundButton.addEventListener("click", () => {
+  clearInterval(interval);
+  funcRoundButton();
+});
+
+
+// функционал кнопок
+const funcstopButton = () => {
+  milsec = 0;
+  sec = 0;
+  min = 0;
+  hour = 0;
+  milsecElement.textContent = "00";
+  secElement.textContent = "00";
+  minElement.textContent = "00";
+  hourElement.textContent = "00";
+  disableBtn()
+};
+
+const funcRoundButton = () => {
+    count++
+    const resultRound = document.createElement("div")
+    resultRound.innerText = `Round ${count}: ${hour > 9 ? hour : "0" + hour}:${min > 9 ? min : "0" + min}:${sec > 9 ? sec : "0" + sec}:${milsec > 9 ? milsec : "0" + milsec}`
+    roundElement.append(resultRound)
+    interval = setInterval(startStopwatch, 10);
+};
+
+const disableBtn = () => {
+    if (disabled) {
+        roundButton.disabled = true
+    }
+}
+disableBtn()
+
+
+// функционал секундомера
 const startStopwatch = () => {
   milsec++;
 
@@ -56,27 +90,29 @@ const startStopwatch = () => {
 
   // подсчёт секунд => минут
   if (sec < 9) {
-    secElement.innerText = "0" + sec
+    secElement.innerText = "0" + sec;
   } else {
-    secElement.innerText = sec
+    secElement.innerText = sec;
   }
   if (sec > 59) {
-    min++
-    minElement.innerText = "0" + min
-    sec = 0
-    secElement.innerText = "0" + sec
+    min++;
+    minElement.innerText = "0" + min;
+    sec = 0;
+    secElement.innerText = "0" + sec;
   }
 
   // подсчёт минуты => часы
   if (min < 9) {
-    minElement.innerText = "0" + min
+    minElement.innerText = "0" + min;
   } else {
-    minElement.innerText = min
+    minElement.innerText = min;
   }
   if (min > 59) {
-    hour++
-    hourElement.innerText = "0" + hour
-    min = 0
-    minElement.innerText = "0" + min
+    hour++;
+    hourElement.innerText = "0" + hour;
+    min = 0;
+    minElement.innerText = "0" + min;
   }
+
+  roundButton.disabled = false
 };
