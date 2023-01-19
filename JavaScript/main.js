14348c2c26a8fbddf7690d52cd9a2cbe23ebe303
artem-xopc <romanovartem.2000@mail.ru>
2023-01-19 13:29:08 +0300

    added the listeners and functional of the pause and stop buttons

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,10 +10,10 @@ const pauseButton = document.querySelector(".pause");
 const stopButton = document.querySelector(".stop");
 
 // Объявляем основные переменные
-let milsec = 00,
-  sec = 00,
-  min = 00,
-  hour = 00,
+let milsec = 0,
+  sec = 0,
+  min = 0,
+  hour = 0,
   interval;
 
 // задаём слушатели
@@ -22,12 +22,29 @@ startButton.addEventListener("click", () => {
   interval = setInterval(startStopwatch, 10);
 });
 
+pauseButton.addEventListener("click", () => {
+    clearInterval(interval)
+});
+
+stopButton.addEventListener("click", () => {
+    clearInterval(interval);
+    milsec = 0
+    sec = 0
+    min = 0
+    hour = 0
+    milsecElement.textContent = "00"
+    secElement.textContent = "00"
+    minElement.textContent = "00"
+    hourElement.textContent = "00"
+});
+
 const startStopwatch = () => {
   milsec++;
+
+  // подсчёт миллисекунд => секунд
   if (milsec < 9) {
     milsecElement.innerText = "0" + milsec;
-  }
-  if (milsec > 9) {
+  } else {
     milsecElement.innerText = milsec;
   }
   if (milsec > 99) {
@@ -35,5 +52,31 @@ const startStopwatch = () => {
     secElement.innerText = "0" + sec;
     milsec = 0;
     milsecElement.innerText = "0" + milsec;
+  }
+
+  // подсчёт секунд => минут
+  if (sec < 9) {
+    secElement.innerText = "0" + sec
+  } else {
+    secElement.innerText = sec
+  }
+  if (sec > 59) {
+    min++
+    minElement.innerText = "0" + min
+    sec = 0
+    secElement.innerText = "0" + sec
+  }
+
+  // подсчёт минуты => часы
+  if (min < 9) {
+    minElement.innerText = "0" + min
+  } else {
+    minElement.innerText = min
+  }
+  if (min > 59) {
+    hour++
+    hourElement.innerText = "0" + hour
+    min = 0
+    minElement.innerText = "0" + min
   }
 };
